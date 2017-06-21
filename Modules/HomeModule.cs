54f23d1de5f2446c48ecaca806dcf8ff38c96266
david rolfs and kimlan nguyen <david+kimlan@>
2017-06-21 10:27:34 -0700

    add method to make title of remedy and aliment name to uppercase

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -39,9 +39,10 @@ namespace Medicine
         string description = Request.Form["remedy-description"];
         string side_effect = Request.Form["remedy-side-effect"];
         string image = Request.Form["remedy-image"];
+        name.ToLower();
+        string nameTitleCasename = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
         int category_id = int.Parse(Request.Form["remedy-category-id"]);
-        Remedy newRemedy = new Remedy(name, description, side_effect, image, category_id);
-        Console.WriteLine(newRemedy.GetImage());
+        Remedy newRemedy = new Remedy(nameTitleCasename, description, side_effect, image, category_id);
         newRemedy.Save();
         Dictionary<string, object> model = new Dictionary<string, object>();
         List<CategoryDisease> AllCategoryDisease = CategoryDisease.GetAll();
@@ -60,7 +61,9 @@ namespace Medicine
         string symptom = Request.Form["disease-symptom"];
         string image = Request.Form["disease-image"];
         int category_id = int.Parse(Request.Form["disease-category-id"]);
-        Disease newDisease = new Disease(name, symptom, image, category_id);
+        name.ToLower();
+        string nameTitleCasename = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+        Disease newDisease = new Disease(nameTitleCasename, symptom, image, category_id);
         newDisease.Save();
         Dictionary<string, object> model = new Dictionary<string, object>();
         List<CategoryDisease> AllCategoryDisease = CategoryDisease.GetAll();
@@ -76,7 +79,9 @@ namespace Medicine
 
       Post["/admin/remedy/category"] = _ =>{
         string name = Request.Form["remedy-category-name"];
-        CategoryRemedy newCategoryRemedy = new CategoryRemedy(name);
+        name.ToLower();
+        string nameTitleCasename = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+        CategoryRemedy newCategoryRemedy = new CategoryRemedy(nameTitleCasename);
         newCategoryRemedy.Save();
         Dictionary<string, object> model = new Dictionary<string, object>();
         List<CategoryDisease> AllCategoryDisease = CategoryDisease.GetAll();
@@ -92,7 +97,9 @@ namespace Medicine
 
       Post["/admin/disease/category"] = _ =>{
         string name = Request.Form["disease-category-name"];
-        CategoryDisease newCategoryDisease = new CategoryDisease(name);
+        name.ToLower();
+        string nameTitleCasename = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+        CategoryDisease newCategoryDisease = new CategoryDisease(nameTitleCasename);
         newCategoryDisease.Save();
         Dictionary<string, object> model = new Dictionary<string, object>();
         List<CategoryDisease> AllCategoryDisease = CategoryDisease.GetAll();
